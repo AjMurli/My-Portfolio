63f902bf399ab83a44f5ff702b867254a90d7dcb
Murli <murlipalav3@gmail.com>
2023-02-15 22:40:38 +0530

    Add new UI Part

--- JS/script.js
+++ JS/script.js
@@ -51,3 +51,93 @@ icon.onclick = function () {
     icon.src = "/IMG/sun.png";
   }
 }
+
+// slider 1
+const slides0 = document.querySelectorAll(".slide0");
+
+counter0 = 0;
+
+slides0.forEach((slide, index) => {
+  slide.style.left = `${index * 56}%`;
+}
+)
+
+const slideImage0 = () => {
+  slides0.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter0 * 100}%)`;
+  })
+};
+
+setInterval(() => {
+  slideImage0();
+  counter0++;
+  for (var i = 0; i <= 4; i++) {
+    setTimeout(() => {
+      slideImage0();
+    }, 1000);
+    if (counter0 == 4) {
+      counter0 = 0;
+    }
+  }
+}, 3200)
+
+
+//  slider 2
+const slides = document.querySelectorAll(".slide");
+
+counter = 0;
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 56}%`;
+}
+)
+
+const slideImage = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  })
+};
+
+setInterval(() => {
+  slideImage();
+  counter++;
+  for (var i = 0; i <= 4; i++) {
+    setTimeout(() => {
+      slideImage1();
+    }, 2000);
+    if (counter == 4) {
+      counter = 0;
+    }
+  }
+}, 3000)
+
+//  slider 3
+const slides1 = document.querySelectorAll(".slide1");
+
+counter1 = 0;
+
+slides1.forEach((slide, index) => {
+  slide.style.left = `${index * 56}%`;
+}
+)
+
+const slideImage1 = () => {
+  slides1.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter1 * 100}%)`;
+  })
+};
+
+setInterval(() => {
+  slideImage1();
+  counter1++;
+  for (var i = 0; i <= 3; i++) {
+    setTimeout(() => {
+      slideImage1();
+    }, 1000);
+
+    if (counter1 == 3) {
+      counter1 = 0;
+    }
+  }
+
+}, 2900)
